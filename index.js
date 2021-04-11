57063cf3dc87143c6b3d4abd201e6be0ecd0d1c2
Denny Mattern <mattern@posteo.de>
2021-04-11 20:53:03 +0200

    refactor and comments

--- index.js
+++ index.js
@@ -13,20 +13,49 @@ class Ipfsify {
     this.run()
   }
 
+  // prepare DOM and start ipfs node
   async run() {
     this.getDOMNodesWithIpfsAttribute()
     this.prevent_default_loading()
 
     this.ipfs = await Ipfs.create({ repo: 'ipfs-' + Math.random() })
-
-    this.all()
   }
 
 
+  // ipfsify all elements
   all() {
     this.images()
+
+    // extend this if other elements should be loaded via ipfs, too
+    // e.g. this.domNodes["SCRIPT"].forEach( this.scripts )
   }
 
+
+  // finds all HTML tags with "ipfs"-attribute
+  getDOMNodesWithIpfsAttribute() {
+    this.domNodes = {}
+
+    let nodes = document.querySelectorAll("[ipfs]")
+    nodes.forEach( node => {
+      if ( !this.domNodes.hasOwnProperty(node.tagName) ) {
+        this.domNodes[node.tagName] = [node]
+      } else {
+        this.domNodes[node.tagName].push(node)
+      }
+    })
+  }
+
+
+  prevent_default_loading() {
+    this.domNodes["IMG"].forEach( this.image_prevent_http_loading )
+
+    // extend this if other elements should be loaded via ipfs, too
+    // e.g. this.domNodes["SCRIPT"].forEach( this.script_prevent_http_loading )
+  }
+
+
+  // START ipfsify images
+  // (ipfsify for other elements will be implemented step by step)
   async images() {
     this.domNodes['IMG'].forEach( async elem => {
       let hash = elem.getAttribute("ipfs")
@@ -42,35 +71,23 @@ class Ipfsify {
   }
 
 
-  getDOMNodesWithIpfsAttribute() {
-    this.domNodes = {}
-
-    let nodes = document.querySelectorAll("[ipfs]")
-    nodes.forEach( node => {
-      if ( !this.domNodes.hasOwnProperty(node.tagName) ) {
-        this.domNodes[node.tagName] = [node]
-      } else {
-        this.domNodes[node.tagName].push(node)
-      }
-    })
-  }
-
-  prevent_default_loading() {
-    this.domNodes["IMG"].forEach( this.image_prevent_http_loading );
-  }
-
+  // stop the browser from loading images traditionally
   image_prevent_http_loading(elem) {
     let src = elem.src
     elem.removeAttribute("src")
     elem.setAttribute("data-src", src)
   }
 
+
+  // rollback: load image traditionally if not possible over ipfs
   load_image_http(elem) {
     let datasrc = elem.dataset.src
     elem.setAttribute("src", datasrc)
   }
+  // END ipfsify images
 
 }
 
-
+// instatiate object and ipfsify all images with "ipfs"-attribute
 const ipfsify = new Ipfsify({})
+ipfsify.all()
